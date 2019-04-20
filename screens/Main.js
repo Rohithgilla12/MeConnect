21885f716672e214fabd3789d8047eac75264726
#Gilla_Dude! <gillarohith1@gmail.com>
2019-04-20 23:33:28 +0530

    UI stuff

--- screens/Main.js
+++ screens/Main.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { StyleSheet, Platform, Image, Text, View , Button} from "react-native";
+import { StyleSheet, Platform, Image, Text, View , Button, FlatList} from "react-native";
 import {f,auth, storage, db} from '../config/config';
 import ActionButton from 'react-native-action-button';
 import {createStackNavigator, createDrawerNavigator,DrawerItems, SafeAreaView, createAppContainer} from 'react-navigation';
@@ -8,9 +8,26 @@ import { Card, ListItem, Icon } from 'react-native-elements';
 import Announcements from './Announcements';
 
 export default class Main extends React.Component{
-    state = {
-        currentUser:null
+
+    constructor(props){
+        super(props);
+        this.state = {
+            currentUser:null,
+            photoFeed : [0,1,2,3,4],
+            refresh: false
+        }
     }
+
+    loadNew = () => {
+        this.setState({
+            refresh: true
+        });
+        this.setState({
+            photoFeed : [5,6,7,8,9],
+            refresh: false
+        })
+    }
+
     static navigationOptions =({navigation})=>({
         title:'Home',
         headerStyle: {
@@ -43,20 +60,38 @@ export default class Main extends React.Component{
         this.props.navigation.navigate('Create')
     }
 
-    takeToAnnoucnements = () =>{
-        this.props.navigation.navigate('Announcements')        
-    }
 
 
     render(){
         const {currentUser} = this.state
         return(
             <View style={styles.container}>
-                <Text>
-                    Hurayyy {currentUser && currentUser.email}!!                    
-                </Text>                
-                <Button title="logout" onPress={this.signOutUser} /> 
-                <Button title="Announcements" onPress={this.takeToAnnoucnements}/> 
+                <FlatList
+                    refreshing = {this.state.refresh}
+                    onRefresh = {this.loadNew}
+                    data = {this.state.photoFeed}
+                    keyExtractor = {(item, index) => index.toString()}
+                    style = {{ flex:1, backgroundColor:"#eee"}}
+                    renderItem = {({item, index}) => (
+                        <View key= {index} style={{width:'100%', overflow:'hidden', marginBottom:5, justifyContent:'space-between', borderColor:'grey', borderBottomWidth:1 }}>
+                            <View style = {{padding:5, width:'100%', flexDirection:'row', justifyContent:'space-between'}}>
+                                <Text>Gilla</Text>
+                                <Text>Time Ago</Text>                                
+                            </View>
+                            <View>
+                                <Image
+                                    source = {{uri: 'https://source.unsplash.com/random/500x'+ Math.floor(Math.random() * 800+ 500).toString() }}
+                                    style = {{resizeMode: 'cover', width:'100%', height: 275}}
+                                />
+                            </View>
+                            <View>
+                                <Text>Caption of the Image goes here!</Text>
+                            </View>
+                            <View style={{flex:0.1}}/>
+                        </View>
+                    )}
+                    />
+
                 <ActionButton
                     buttonColor="rgba(231,76,60,1)"
                     onPress={() => { 
@@ -71,8 +106,7 @@ export default class Main extends React.Component{
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+
     },
     textInput:{
         height: 40,
