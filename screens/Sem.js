eac6f1b0b63b039d48672a6d6f43b66355ee152f
#Gilla_Dude! <gillarohith1@gmail.com>
2019-04-28 18:00:35 +0530

    Cards Layout

--- screens/Sem.js
+++ screens/Sem.js
@@ -1,6 +1,21 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import {Icon } from 'react-native-elements';
+import React, {
+    Component
+} from 'react';
+import {
+    View,
+    Text,
+    ScrollView,
+    StyleSheet,
+    TouchableHighlight
+} from 'react-native';
+import {
+    Card,
+    ListItem,
+    Button,
+    Icon
+} from 'react-native-elements'
+
+
 
 class Sem extends Component {
 
@@ -23,9 +38,140 @@ class Sem extends Component {
 
     render() {
         return(
-            <View><Text>Semester Component</Text></View>
+            <ScrollView>                
+                <View style = {styles.boxOddEven}>
+                    <Card
+                        // image={require('../images/sem1.jpg)}
+                        containerStyle = {styles.cardShow}
+                        title='Semester 1'>
+                        <Text style={{marginBottom: 10}}>
+                        </Text>
+                        <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            // title='VIEW'
+                             />
+                    </Card>
+                    <Card
+                        // image={require('../images/sem1.jpg)}
+                        containerStyle = {styles.cardShow}
+                        title='Semester 2'>
+                        <Text style={{marginBottom: 10}}>
+                        </Text>
+                        <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            // title='VIEW'
+                             />
+                    </Card>
+                </View>
+                <View style = {styles.boxOddEven}>
+                    <Card
+                        // image={require('../images/sem1.jpg)}
+                        containerStyle = {styles.cardShow}
+                        title='Semester 3'>
+                        <Text style={{marginBottom: 10}}>
+                        </Text>
+                        <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            // title='VIEW'
+                             />
+                    </Card>
+                    <Card
+                        containerStyle = {styles.cardShow}
+                        // image={require('../images/sem1.jpg)}
+                        title='Semester 4'>
+                        <Text style={{marginBottom: 10}}>
+                        </Text>
+                        <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            // title='VIEW'
+                             />
+                    </Card>
+                </View>
+                <View style = {styles.boxOddEven}>
+                    <Card
+                        containerStyle = {styles.cardShow}
+                        // image={require('../images/sem1.jpg)}
+                        title='Semester 5'>
+                        <Text style={{marginBottom: 10}}>
+                        </Text>
+                        <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            
+                             />
+                    </Card>
+                    <Card
+                        containerStyle = {styles.cardShow}
+                        // image={require('../images/sem1.jpg)}
+                        title='Semester 6'>
+                        <Text style={{marginBottom: 10}}>
+                        </Text>
+                        <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            
+                             />
+                    </Card>
+                </View>
+                <View style = {styles.boxOddEven}>
+                    <Card
+                        // image={require('../images/sem1.jpg)}
+                        containerStyle = {styles.cardShow}
+                        title='Semester 7'>
+                        <Text style={{marginBottom: 10}}>
+                        </Text>
+                        <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            // title='VIEW'
+                             />
+                    </Card>
+                    <Card
+                        containerStyle = {styles.cardShow}
+                        // image={require('../images/sem1.jpg)}
+                        title='Semester 8'>
+                        <Text style={{marginBottom: 10}}>
+                        </Text>
+                        <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            // title='VIEW'
+                             />
+                    </Card>
+                </View>
+            </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    boxOddEven:{
+        flex: 1,
+        flexDirection : 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        padding: 20
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 15
+      },
+      cardShow : {
+        width: 180
+    }
+})
 
 export default Sem;
