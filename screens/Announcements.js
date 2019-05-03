3f9ac5256b9fab815a0b3f9c918ba8d1589d844a
#Gilla_Dude! <gillarohith1@gmail.com>
2019-05-03 22:42:35 +0530

    Formatting

--- screens/Announcements.js
+++ screens/Announcements.js
@@ -1,79 +1,59 @@
 import React from 'react'
-import { View, Text, StyleSheet,ActivityIndicator, FlatList } from 'react-native'
-import {f,auth, storage, db} from '../config/config';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList
+} from 'react-native'
+import {
+  f,
+  auth,
+  storage,
+  db
+} from '../config/config';
 import ActionButton from 'react-native-action-button';
-import { Card, ListItem, Icon } from 'react-native-elements';
+import {
+  Card,
+  ListItem,
+  Icon
+} from 'react-native-elements';
 
 
 export default class Announcements extends React.Component {
     constructor() {
-        super();
-        this.ref = db.collection('announcements');
-        this.unsubscribe = null;
-        this.state = {
-            textInput: '',
-            loading: true,
-            announcements: [],
-        };
+      super();
+      this.unsubscribe = null;
+      this.state = {
+        textInput: '',
+        loading: true,
+        announcements: [],
+      };
     }
 
-    static navigationOptions =({navigation})=>({
-      title:'Announcements',
+    static navigationOptions = ({
+      navigation
+    }) => ({
+      title: 'Announcements',
       headerStyle: {
-          backgroundColor: "#355876"
+        backgroundColor: "#355876"
       },
       headerTintColor: '#fff',
-      headerLeft : <Icon 
-                          name='menu' 
-                          size={30}
-                          color='white'
-                          onPress={()=>navigation.toggleDrawer()}
-                  />
-  })
-
-    // componentDidMount() {
-    //     this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate) 
-    // }
-    
-    // componentWillUnmount() {
-    //     this.unsubscribe();
-    // }
-
-    onCollectionUpdate = (querySnapshot) => {
-        const announcements = [];
-        querySnapshot.forEach((doc) => {            
-          const { description, user } = doc.data();
-          announcements.push({
-            key: doc.id,
-            doc, // DocumentSnapshot
-            description,
-            user,
-          });
-        });
-        this.setState({ 
-        announcements,
-          loading: false,
-       });
-       console.log(this.state.announcements)
+      headerLeft: < Icon
+      name = 'menu'
+      size = {
+        30
       }
+      color = 'white'
+      onPress = {
+        () => navigation.toggleDrawer()
+      }
+      />
+    })
   render() {
     return (
             <View style={styles.container}>
             <Text>Announcements Route</Text> 
-            {/* <FlatList
-                data={this.state.announcements}
-                renderItem={
-                  ({item}) => 
-                  <Card
-                  cardElevation={2}
-                  cardMaxElevation={2}
-                  cornerRadius={5}
-                  >
-                    <Text>{item.description}</Text>
-                    <Text>{item.user}</Text>
-                  </Card>
-                  }
-            /> */}
             <ActionButton
                 buttonColor="rgba(231,76,60,1)"
                 onPress={() => { 
